45e5be90d312358cbd544e53c7de46e65444787d
Elliott Zaki <elliottzaki@Elliotts-MacBook-Air.local>
2022-08-31 18:16:51 +0100

    arrow functions

---
+++ arrow_functions/index.js
@@ -0,0 +1,27 @@
+// Long format:
+// function greeting(message) {
+//   return alert(`${message}, everyone!`);
+// }
+
+// greeting('Good afternoon');
+
+// One-liner arrow function syntax:
+// If it is one argument, you do not require a paranthesis.
+// If it is one statement that you returning, you do not need the return key word.
+
+// greeting = (message) => alert(`${message}, everyone!`);
+// greeting('Good afternoon');
+
+// If you require an if else statement than use the curley brakcets like so:
+
+createblog = (title, body) => {
+  if(!title) {
+    throw new error('A title is required');
+  }
+  if(!body) {
+    throw new error('Body cannot be empty');
+  }
+  return alert(`${title} - ${body}`);
+}
+
+createblog('Javascript_Fundamentals', 'This is a Javascript fundamental blog');
